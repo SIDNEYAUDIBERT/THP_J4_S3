c87a39e84b3d5cc503d38fbaddeb6869aa5b414f
SIDNEYAUDIBERT <47211505+SIDNEYAUDIBERT@users.noreply.github.com>
2021-04-23 08:38:03 +0200

    Update speccrypto.rb

--- spec/speccrypto.rb
+++ spec/speccrypto.rb
@@ -2,10 +2,7 @@ require_relative '../lib/exo01.rb'
 
 describe "the dark trader function" do
     it "should return an answer" do
-        expect(scrapping).not_to be_nil
+        expect(scrapper).not_to be_nil
+        expect(scrapper).to be_a(Nokogiri::HTML::Document)
     end
-    it "should be an Hash" do
-        expect(scrapping).to be_a(Hash)
-    end
-
 end
